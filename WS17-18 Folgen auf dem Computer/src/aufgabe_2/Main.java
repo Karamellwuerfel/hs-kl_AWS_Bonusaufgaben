3be5d942b1841f8e85b7b5fb7f885d8c38694c08
Philipp Dalheimer - DesktopPC <philippdalheimer@gmail.com>
2017-12-10 22:08:45 +0100

    Aufgabe 2

--- WS17-18 Folgen auf dem Computer/src/aufgabe_2/Main.java
+++ WS17-18 Folgen auf dem Computer/src/aufgabe_2/Main.java
@@ -2,8 +2,11 @@ package aufgabe_2;
 
 public class Main {
 
+	static double b = 2 * Math.sqrt(3);
+	static double a = 3;
+	
 	public Main() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
@@ -14,18 +17,14 @@ public class Main {
 	
 	public static void Pi(){
 		
-		double a_k = 2 * Math.sqrt(3);
-		double b_k = 3;
-		
-		while(a_k < b_k) {
+		while( a != b){
 			
-			b_k = 2 / (Math.pow(b_k, -1) + (Math.pow(a_k, -1)));
-			a_k = Math.sqrt((a_k)*b_k);
-			
-		}
+	      b = 2 * b * a /( b + a);
+	      a = Math.sqrt( b * a);
+	      
+	    }
 		
-
-		
+		System.out.println(a);
 	}
 
 }
